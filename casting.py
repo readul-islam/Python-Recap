234630eaf56070390341a71e58d7db48344fbef8
readul-islam <developer.readul@gmail.com>
2024-03-12 12:27:02 +0600

    casting, case-sensitive, type checking,  done

---
+++ casting.py
@@ -0,0 +1,27 @@
+
+# type casting
+ 
+b = '3'
+
+x= str(b)
+print(x)
+
+y = int(b)
+print(y)
+
+z = float(b)
+
+print(z)
+
+# which type or get type of our value
+
+x= 5
+y = 'readul islam'
+print(type(x))
+print(type(y))
+
+
+# case-sensitive
+
+a = 4
+A = 'salary'
