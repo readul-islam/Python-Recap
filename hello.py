0ee787768fefead5024ec90a673617b0dbbcebe8
readul-islam <developer.readul@gmail.com>
2024-03-12 12:18:31 +0600

    i forgot some syntax so i need to recape aging python

---
+++ hello.py
@@ -0,0 +1,18 @@
+import sys
+
+print(sys.version);
+
+# indentation most important in python
+if 5 > 2:
+     print("5 is gater than 2")
+     
+     
+# python variable 
+
+x = 5
+y = 'Hello, World!'
+
+x = y
+
+
+print(x, y)
