08d56b9e068102a727eb878506175a7e08ba8d8f
readul-islam <developer.readul@gmail.com>
2024-03-12 19:18:36 +0600

    stirng check method in and not in done

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+import random;
+
+print(random.randrange(0,6))
+# Data types 
+
+x = "Hello , world!" #str
+x = 20 
+x = 20.5 #float
+x = 1j #complex
+x = ["apple", "orange"] #list
+x = ("tuple", "banana") #tuple
+x = range(6) 
+
+for x in "banana":
+    print(x)
+
+print(len("ab"));
+
+
+txt ="my name is readul  islam akash . i'm from bangladesh and where are you from?"
+
+print("ami" not in txt)
+
+
+if "kodi" not in txt:
+    print("nai")
